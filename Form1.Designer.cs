d0588e1c8c16e39f8e4b21f52d9ce9a2210d3abb
phanngocanh-2005 <phanngocanh20022005@gmail.com>
2024-12-10 20:41:40 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnPlayPause = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.listBoxPlaylist = new System.Windows.Forms.ListBox();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnOpen.Location = new System.Drawing.Point(584, 25);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(97, 43);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = false;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnPlayPause
+            // 
+            this.btnPlayPause.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnPlayPause.Location = new System.Drawing.Point(584, 104);
+            this.btnPlayPause.Name = "btnPlayPause";
+            this.btnPlayPause.Size = new System.Drawing.Size(97, 41);
+            this.btnPlayPause.TabIndex = 2;
+            this.btnPlayPause.Text = "Play/Pause";
+            this.btnPlayPause.UseVisualStyleBackColor = false;
+            this.btnPlayPause.Click += new System.EventHandler(this.btnPlayPause_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnExit.Location = new System.Drawing.Point(584, 196);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(97, 40);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnRemove.Location = new System.Drawing.Point(584, 271);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(97, 44);
+            this.btnRemove.TabIndex = 4;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = false;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // listBoxPlaylist
+            // 
+            this.listBoxPlaylist.BackColor = System.Drawing.Color.White;
+            this.listBoxPlaylist.FormattingEnabled = true;
+            this.listBoxPlaylist.ItemHeight = 16;
+            this.listBoxPlaylist.Location = new System.Drawing.Point(24, 419);
+            this.listBoxPlaylist.Name = "listBoxPlaylist";
+            this.listBoxPlaylist.Size = new System.Drawing.Size(240, 148);
+            this.listBoxPlaylist.TabIndex = 5;
+            this.listBoxPlaylist.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(24, 25);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(413, 290);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(993, 593);
+            this.Controls.Add(this.listBoxPlaylist);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPlayPause);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "Video Player";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnPlayPause;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.ListBox listBoxPlaylist;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace App
+{
+    public partial class Form1 : Form
+    {
+        private List<string> videoPaths = new List<string>();
+        private string playlistFile = "playlist.txt";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadPlaylist();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnPlayPause.Enabled = false;
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Video Files|*.mp4;*.avi;*.mkv|All Files|*.*";
+                openFileDialog.Multiselect = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string file in openFileDialog.FileNames)
+                    {
+                        videoPaths.Add(file);
+                        listBoxPlaylist.Items.Add(Path.GetFileName(file));
+                    }
+
+                    SavePlaylist();
+                }
+            }
+        }
+
+        private void btnPlayPause_Click(object sender, EventArgs e)
+        {
+            if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.pause();
+                btnPlayPause.Text = "Play";
+            }
+            else
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                btnPlayPause.Text = "Pause";
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (listBoxPlaylist.SelectedIndex >= 0)
+            {
+
+                int selectedIndex = listBoxPlaylist.SelectedIndex;
+                listBoxPlaylist.Items.RemoveAt(selectedIndex);
+                videoPaths.RemoveAt(selectedIndex);
+                SavePlaylist();
+            }
+            else
+            {
+                MessageBox.Show("Please select a video to delete");
+            }
+        }
+        private void SavePlaylist()
+        {
+            try
+            {
+                File.WriteAllLines(playlistFile, videoPaths);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error when saving list: {ex.Message}");
+            }
+        }
+
+        private void LoadPlaylist()
+        {
+            if (File.Exists(playlistFile))
+            {
+                try
+                {
+
+                    videoPaths = new List<string>(File.ReadAllLines(playlistFile));
+                    foreach (var video in videoPaths)
+                    {
+                        listBoxPlaylist.Items.Add(Path.GetFileName(video));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error when saving list: {ex.Message}");
+                }
+            }
+
+        }
+
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxPlaylist.SelectedIndex >= 0)
+            {
+                string selectedVideoPath = videoPaths[listBoxPlaylist.SelectedIndex];
+                axWindowsMediaPlayer1.URL = selectedVideoPath;
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                btnPlayPause.Text = "Pause";
+                btnPlayPause.Enabled = true;
+            }
+        }
+
+
+
+    }
+}
+
